ce3a30d5f3eb529a2f2ba33dadcc78d50b2760bd
Chengka <european@iqing.com>
2017-11-16 13:54:57 +0800

    初始化

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,17 @@
-from fabric.api import local
+from fabric.api import *
+from fabric.contrib.console import confirm
 
-def prepare_deploy():
+env.hosts = ['root@47.52.235.110']
+
+def push():
     local("git add -p && git commit")
     local("git push")
+
+def deploy():
+    code_dir = '/www/maimoe'
+    with settings(warn_only=True):
+        if run("test -d %s" % code_dir).failed:
+            run("git clone git@github.com:bydmm/maimoe.git %s" % code_dir)
+    with cd(code_dir):
+        run("git pull")
+        run("service nginx restart")
